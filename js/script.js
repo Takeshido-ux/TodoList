996ba34ed49496c6099009904afe1af827a8dc84
Takeshido-ux <isma555555655@gmail.com>
2021-11-27 15:07:44 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $('#addBtn').click(function(){
         })
     }
     if (textInpt){
-        $('ul').append('<li id="'+ id +'" class="list-group-item task align-items-center justify-content-between"><p>'+ textInpt +'</p><div><button class="btn btn-sm btn-icon btn-secondary btnChange" type="button"><i class="fa fa-pencil-alt"></i> <span class="sr-only">Edit</span></button><button class="btn btn-sm btn-icon btn-secondary btnRemove" type="button"><i class="fa fa-pencil-alt"></i> <span class="sr-only">Remove</span></button></div></li>')
+        $('ul').append('<li id="'+ id +'" class="list-group-item task align-items-center justify-content-between"><p>'+ textInpt +'</p><div><button class="btn btn-sm btn-icon btn-secondary btnChange" type="button"><i class="fa fa-pencil-alt"></i> <span class="sr-only">Edit</span></button><button class="btn btn-sm btn-icon btn-secondary btnRemove" type="button"><i class="far fa-trash-alt"></i> <span class="sr-only">Remove</span></button></div></li>')
     }
     else{
         alert('Введите текст')
@@ -24,32 +24,24 @@ $('ul').on('click', '.btnRemove', function(){
     let delText = confirm('Вы уверены ?');
     let idTask = $(this).parents('li').attr('id');
     let arr = `https://jsonplaceholder.typicode.com/todos/${idTask}`;
+    if(delText == false){
+        return;
+    }
+    else{
+        $(this).parents('li').remove();;
+    }
     if ($('.syncWithServer').hasClass('on')){
         $.ajax({
             url: arr,
             type: 'DELETE',
         })
     }
-    if(delText == true){
-        $(this).parents('li').remove();
-    }
-    else{
-        return;
-    }
 })
+
 $('ul').on('click', '.btnChange', function(){
     let changeText = prompt('Введите текст:');
     let idTask = $(this).parents('li').attr('id');
     let arr = `https://jsonplaceholder.typicode.com/todos/${idTask}`;
-    if ($('.syncWithServer').hasClass('on')){
-        $.ajax({
-            url: arr,
-            type: 'PUT',
-            data:{
-                "title": changeText,
-            }
-        })
-    }
     if (changeText == null){
         return;
     }
@@ -58,6 +50,15 @@ $('ul').on('click', '.btnChange', function(){
     }
     else{
         $(this).parents('li').find('p').text(changeText);
+    }
+    if ($('.syncWithServer').hasClass('on')){
+        $.ajax({
+            url: arr,
+            type: 'PUT',
+            data:{
+                "title": changeText,
+            }
+        })
     }
 })
 
@@ -71,7 +72,7 @@ $('.syncWithServer').click(function(){
         success: function(data){
             data.length = data.length / 4;
             $.each( data , function(item){
-                $('ul').append('<li id="'+ data[item].id +'" class="list-group-item task align-items-center justify-content-between"><p>'+ data[item].title +'</p><div><button class="btn btn-sm btn-icon btn-secondary btnChange" type="button"><i class="fa fa-pencil-alt"></i> <span class="sr-only">Edit</span></button><button class="btn btn-sm btn-icon btn-secondary btnRemove" type="button"><i class="fa fa-pencil-alt"></i> <span class="sr-only">Remove</span></button></div></li>');
+                $('ul').append('<li id="'+ data[item].id +'" class="list-group-item task align-items-center justify-content-between"><p>'+ data[item].title +'</p><div><button class="btn btn-sm btn-icon btn-secondary btnChange" type="button"><i class="fa fa-pencil-alt"></i> <span class="sr-only">Edit</span></button><button class="btn btn-sm btn-icon btn-secondary btnRemove" type="button"><i class="fa fa-trash-alt"></i> <span class="sr-only">Remove</span></button></div></li>');
                 $('.syncWithServer').addClass('on');
             })
         }

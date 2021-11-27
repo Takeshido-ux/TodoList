bc637c05d6094581cd98f672d683a5ff06567bd6
Takeshido-ux <isma555555655@gmail.com>
2021-11-27 14:40:45 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,82 @@
+let id = 201;
+$('#addBtn').click(function(){
+    let textInpt = $('#textInpt').val();
+    if ($('.syncWithServer').hasClass('on')){
+        $.ajax({
+            url: 'https://jsonplaceholder.typicode.com/todos',
+            type: 'POST',
+            data:{ 
+                "title": `${textInpt}`,
+            }
+        })
+    }
+    if (textInpt){
+        $('ul').append('<li id="'+ id +'" class="list-group-item task align-items-center justify-content-between"><p>'+ textInpt +'</p><div><button class="btn btn-sm btn-icon btn-secondary btnChange" type="button"><i class="fa fa-pencil-alt"></i> <span class="sr-only">Edit</span></button><button class="btn btn-sm btn-icon btn-secondary btnRemove" type="button"><i class="fa fa-pencil-alt"></i> <span class="sr-only">Remove</span></button></div></li>')
+    }
+    else{
+        alert('Введите текст')
+    }
+    $('#textInpt').val('');
+    console.log(id);
+    id = id + 1;
+})
+$('ul').on('click', '.btnRemove', function(){
+    let delText = confirm('Вы уверены ?');
+    let idTask = $(this).parents('li').attr('id');
+    let arr = `https://jsonplaceholder.typicode.com/todos/${idTask}`;
+    if ($('.syncWithServer').hasClass('on')){
+        $.ajax({
+            url: arr,
+            type: 'DELETE',
+        })
+    }
+    if(delText == true){
+        $(this).parents('li').remove();
+    }
+    else{
+        return;
+    }
+})
+$('ul').on('click', '.btnChange', function(){
+    let changeText = prompt('Введите текст:');
+    let idTask = $(this).parents('li').attr('id');
+    let arr = `https://jsonplaceholder.typicode.com/todos/${idTask}`;
+    if ($('.syncWithServer').hasClass('on')){
+        $.ajax({
+            url: arr,
+            type: 'PUT',
+            data:{
+                "title": changeText,
+            }
+        })
+    }
+    if (changeText == null){
+        return;
+    }
+    else if (changeText === ''){
+        alert('Вы не ввели текст:');
+    }
+    else{
+        $(this).parents('li').find('p').text(changeText);
+    }
+})
+
+
+
+
+$('.syncWithServer').click(function(){
+    $.ajax({
+        url: 'https://jsonplaceholder.typicode.com/todos/',
+        type: 'GET',
+        success: function(data){
+            data.length = data.length / 4;
+            $.each( data , function(item){
+                $('ul').append('<li id="'+ data[item].id +'" class="list-group-item task align-items-center justify-content-between"><p>'+ data[item].title +'</p><div><button class="btn btn-sm btn-icon btn-secondary btnChange" type="button"><i class="fa fa-pencil-alt"></i> <span class="sr-only">Edit</span></button><button class="btn btn-sm btn-icon btn-secondary btnRemove" type="button"><i class="fa fa-pencil-alt"></i> <span class="sr-only">Remove</span></button></div></li>');
+                $('.syncWithServer').addClass('on');
+            })
+        }
+    })
+})
+
+    
+
